11996e46311843173684bdf86a35f7da783825eb
Akkhil Coding Hub <113294188+TheGamingWinner@users.noreply.github.com>
2024-07-20 15:35:42 +0530

    test commit

--- config.h
+++ config.h
@@ -62,12 +62,12 @@ static const char unknown_str[] = "n/a";
  * wifi_essid          WiFi ESSID                      interface name (wlan0)
  */
 
-static const char vol[]         = "[ `amixer sget Master | tail -n 1 | awk '{print $6;}'` = \"[on]\" ] \
-                                   && printf \"`amixer sget Master | tail -n 1 | awk '{print $5;}' | grep -Po '\\[\\K[^%]*'`%%\" \
+static const char vol[]         = "[ `amixer sget Master | tail -n 1 | awk -F'[][]' '{print $4}'` = \"on\" ] \
+                                   && printf \"`amixer sget Master | tail -n 1 | awk -F'[][]' '{print $2}'`%%\" \
                                    || printf 'Off'";
 
-static const char mic[]         = "[ `amixer sget Capture | tail -n 1 | awk '{print $6;}'` = \"[on]\" ] \
-                                   && printf \"`amixer sget Capture | tail -n 1 | awk '{print $5;}' | grep -Po '\\[\\K[^%]*'`%%\" \
+static const char mic[]         = "[ `amixer sget Capture | tail -n 1 | awk -F'[][]' '{print $4}'` = \"on\" ] \
+                                   && printf \"`amixer sget Capture | tail -n 1 | awk -F'[][]' '{print $2}'`%%\" \
                                    || printf 'Off'";
 
 static const struct arg args[] = {
@@ -76,10 +76,9 @@ static const struct arg args[] = {
         { ram_used,             " %s",         NULL },
         { ram_perc,             "(%s%%) ",      NULL },
         { run_command,          " %s%% ",      "xbacklight -get" },
-        { battery_perc,         " %s%%",       "BAT0" },
-        { battery_state,        "(%s) ",        "BAT0" },
+        { battery_perc,         " %s%%",       "BAT1" },
+        { battery_state,        "(%s) ",        "BAT1" },
         { run_command,          " %s ",        vol },
         { run_command,          " %s ",        mic },
-        { keymap,               " %s ",        NULL },
-        { datetime,             " %s",         "%a %F %T" }, /* Date time with this format: Day name YYYY-MM-DD 18:00:00 */
+        { datetime,             " %s",         "%a %F %r" }, /* Date time with this format: Day name YYYY-MM-DD 06:00:00 PM */
 };
